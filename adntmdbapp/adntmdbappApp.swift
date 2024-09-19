1a4495555a5d96629e0324fb9841da0e071af6ec
androiddevnotes <awesomedevnotes@gmail.com>
2024-09-19 18:04:57 +0100

    Integrate Atlantis for network traffic monitoring via Proxyman

--- adntmdbapp/adntmdbappApp.swift
+++ adntmdbapp/adntmdbappApp.swift
@@ -1,7 +1,12 @@
 import SwiftUI
-
+import Atlantis
 @main
 struct adntmdbappApp: App {
+  
+  init() {
+    // 2. Connect to your Macbook
+    Atlantis.start(hostName: "asks-macbook-pro.local.")
+  }
     var body: some Scene {
         WindowGroup {
             ContentView()
