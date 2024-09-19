a320e186191c8cd41163ac4053c8383d811fb767
androiddevnotes <awesomedevnotes@gmail.com>
2024-09-19 22:44:41 +0100

    Refactored genre name-to-id mapping to use GenreMapping for improved modularity

--- adntmdbapp/NetworkManager.swift
+++ adntmdbapp/NetworkManager.swift
@@ -49,7 +49,8 @@ class NetworkManager: ObservableObject {
       "\(baseURL)/discover/movie?api_key=\(apiKey)&page=\(currentPage)&sort_by=\(currentCategory.rawValue)"
 
     if !selectedGenres.isEmpty {
-      let genreIds = selectedGenres.compactMap { genreNameToId[$0] }.joined(separator: ",")
+      let genreIds = selectedGenres.compactMap { GenreMapping.genreNameToId[$0] }.joined(
+        separator: ",")
       urlString += "&with_genres=\(genreIds)"
     }
 
@@ -180,27 +181,6 @@ class NetworkManager: ObservableObject {
     }
   }
 
-  private let genreNameToId = [
-    "Action": "28",
-    "Adventure": "12",
-    "Animation": "16",
-    "Comedy": "35",
-    "Crime": "80",
-    "Documentary": "99",
-    "Drama": "18",
-    "Family": "10751",
-    "Fantasy": "14",
-    "History": "36",
-    "Horror": "27",
-    "Music": "10402",
-    "Mystery": "9648",
-    "Romance": "10749",
-    "Science Fiction": "878",
-    "TV Movie": "10770",
-    "Thriller": "53",
-    "War": "10752",
-    "Western": "37",
-  ]
 }
 
 extension NetworkManager {
