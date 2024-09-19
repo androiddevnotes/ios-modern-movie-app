5a61a838cedd034d8e5b9664c29ee7d6aec6d97c
androiddevnotes <awesomedevnotes@gmail.com>
2024-09-19 18:25:26 +0100

    Refactored ContentView to use NavigationLink directly for movie details navigation

--- adntmdbapp/ContentView.swift
+++ adntmdbapp/ContentView.swift
@@ -8,29 +8,19 @@ struct ContentView: View {
         NavigationView {
             List {
                 ForEach(networkManager.movies) { movie in
-                    HStack {
-                        networkManager.posterImage(for: movie)
-                            .frame(width: 100, height: 150)
-                        VStack(alignment: .leading) {
-                            Text(movie.title)
-                                .font(.headline)
-                            Text(movie.overview)
-                                .font(.subheadline)
-                                .lineLimit(3)
+                    NavigationLink(destination: MovieDetailView(movie: movie)) {
+                        HStack {
+                            networkManager.posterImage(for: movie)
+                                .frame(width: 100, height: 150)
+                            VStack(alignment: .leading) {
+                                Text(movie.title)
+                                    .font(.headline)
+                                Text(movie.overview)
+                                    .font(.subheadline)
+                                    .lineLimit(3)
+                            }
                         }
                     }
-                    .onTapGesture {
-                        selectedMovie = movie
-                    }
-                    .background(
-                        NavigationLink(destination: MovieDetailView(movie: selectedMovie ?? movie), isActive: Binding(
-                            get: { selectedMovie == movie },
-                            set: { if !$0 { selectedMovie = nil } }
-                        )) {
-                            EmptyView()
-                        }
-                        .hidden()
-                    )
                 }
                 if networkManager.currentPage <= networkManager.totalPages {
                     ProgressView()
